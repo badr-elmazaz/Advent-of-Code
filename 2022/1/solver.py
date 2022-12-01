7bd6e787e8c26686fb1d636425ca3131d5767783
Badr El Mazaz <badr.elmazaz@eng.it>
2022-12-01 23:35:25 +0100

    day 1 fixing

--- 2022/1/solver.py
+++ 2022/1/solver.py
@@ -8,7 +8,27 @@ def get_input():
 
 
 
-def solve():
+def solve2():
+    calories = get_input()
+    top=3
+    total_calories = []
+    sum=0
+    for i in range(len(calories)):
+        if(i==len(calories)-1 or calories[i]=="\n"):
+            if (i==len(calories)):
+                sum+=int(calories[i].replace("\n", ""))
+            total_calories.append(sum)
+            sum=0
+        else:
+            sum+=int(calories[i].replace("\n", ""))
+    total_calories.sort(reverse=True)
+    answer = 0
+    for i in range(top):
+        answer+=total_calories[i]
+    print(answer)
+
+
+def solve1():
     calories = get_input()
     elf_max = 1
     current_elf = 1
@@ -31,4 +51,5 @@ def solve():
 
 
 if __name__ == "__main__":
-    solve()
+    solve1()
+    solve2()
